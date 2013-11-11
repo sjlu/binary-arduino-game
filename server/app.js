c5f05b806191261324b5fa567e68c66054637430
Shyamal Ruparel <shy@shyruparel.com>
2013-11-10 21:32:04 -0500

    Added scoreboard and background image

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
+
 var requirejs = require('requirejs');
-requirejs.config({nodeRequire: require});
 
-requirejs(['serial', 'web', 'socket'],function(serial, web, socket) {
-  var player1 = serial('/dev/tty.usbmodem1d1111').connect(socket);
-  var player2 = serial('/dev/tty.usbmodem1d1121').connect(socket);
 
-  var resetAll = function() {
-    player1.write("r");
-    player2.write("r");
-  };
+requirejs.config({
+    nodeRequire: require
+});
+//var twilio = require('twilio');
+requirejs(['serial', 'web', 'socket'], function(serial, web, socket) {
+    //  var player1 = serial('/dev/tty.usbmodem1d1111').connect(socket);
+    //  var player2 = serial('/dev/tty.usbmodem1d1121').connect(socket); //Steve
+    
+    var player1 = serial('/dev/ttyACM0').connect(socket);
+    var player2 = serial('/dev/ttyACM1').connect(socket); //Shy
 
-  socket.sockets.on('connection', function(socket) {
-    socket.on("reset", function(data) {
-      resetAll();
+    var resetAll = function() {
+        player1.write("r");
+        player2.write("r");
+    };
+
+    socket.sockets.on('connection', function(socket) {
+        socket.on("reset", function(data) {
+            resetAll();
+        });
     });
-  });
 
-  web.listen(8099);
+    web.listen(8099);
 });
--- server/public/scripts/main.js
+++ server/public/scripts/main.js
@@ -1,10 +1,42 @@
 var isRunning = false;
 
+// var client = new twilio.RestClient('AC831f5189f378fe2ec46eb761e911abe8', '289993376273c3ef4fae1e0ad51cc3f2');
+
+// function text(){
+//   client.sms.messages.create({
+//   to:'+15135184527',
+//   from:'+12709443699',
+//   body:"The chlorine in fresh tap water irritates sensitive parts of the cat's nose. Let tap water sit for 24 hours before giving it to a cat."
+//   }, function(error, message) {
+//   // The HTTP request to Twilio will run asynchronously. This callback
+//   // function will be called when a response is received from Twilio
+//   // The "error" variable will contain error information, if any.
+//   // If the request was successful, this value will be "falsy"
+//   if (!error) {
+//   // The second argument to the callback will contain the information
+//   // sent back by Twilio for the request. In this case, it is the
+//   // information about the text messsage you just sent:
+//   console.log('Success! The SID for this SMS message is:');
+//   console.log(message.sid);
+   
+//   console.log('Message sent on:');
+//   console.log(message.dateCreated);
+//   }
+//   else {
+//   console.log('Oops! There was an error.');
+//   }
+//   });
+// }
 require(["jquery"], function($) {
   var number = 0;
+  
+  var scoreone = 0;
+  var scoretwo= 0;
   var setNumber = function() {
     number = Math.floor((Math.random()*128));
     $('#number').html(number);
+    $('#scoreone').html(Number(scoreone).toString(2));
+    $('#scoretwo').html(Number(scoretwo).toString(2));
   };
   setNumber();
 
@@ -14,9 +46,16 @@ require(["jquery"], function($) {
     
     var binary = parsed.states.join('');
     var integer = parseInt(binary, 2);
-
+    // text();
     if (integer === number) {
-      alert("Player " + parsed.controller + " Wins!");
+
+      if (parseInt(parsed.controller) == 1){
+        scoreone = scoreone + 1;
+        alert("Player 01 Wins!");
+      }else{
+        scoretwo = scoretwo + 1;
+        alert("Player 10 Wins!");
+      }
       setNumber();
       socket.emit("reset", "reset");
     }
